571ca80d1d4d4eba83283cf38d1e1983deddc251
isroil <5085624@mail.ru>
2022-12-05 06:34:55 +0500

    alert

--- settingIphone/ViewController.swift
+++ settingIphone/ViewController.swift
@@ -21,16 +21,23 @@ class ViewController: UIViewController {
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
+    
 
 }
 extension ViewController:  UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return rainbow.count
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let alert = UIAlertController(title: "title", message: rainbow[indexPath.row].capitalized, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "ok", style: .default))
+        self.present(alert, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
-        cell.mainLabel?.text = rainbow[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = rainbow[indexPath.row].capitalized
         return cell
     }
 }
