712e7e96680446fb27967fd180638139ee21b699
isroil <5085624@mail.ru>
2022-12-05 05:36:09 +0500

    tableview

--- settingIphone/ViewController.swift
+++ settingIphone/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    let rainbow: [String] = ["isroil","bahtiyor","jamshid"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,17 +18,20 @@ class ViewController: UIViewController {
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
 
 }
-extension ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let rainbow: [UIColor] = [.red, .yellow, .green, .orange, .blue, .purple, .magenta]
+extension ViewController:  UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return rainbow.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        cell.mainLabel?.text = rainbow[indexPath.row]
+        return cell
     }
 }
 
